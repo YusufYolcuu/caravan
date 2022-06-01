3132698425b10f4ecd47693bbcfbbb00452559cb
YusufYolcuu <yyolcu634@gmail.com>
2022-06-01 15:42:41 +0300

    exterior-part

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,51 @@
-let explain = document.querySelector("#explain")
-let n = document.querySelector("#new");
+// interior nav slide part function
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
 
 
-// function ff() {
-//     document.getElementById("products").style.display = "none";
-//     document.getElementById("new").innerHTML = "merhaba";
+// Fonksiyonlar iki ayrı değişken alıyor. o yüzden iki tane ayrı fonksiyon var
+// daha sonra bir js veri yapısı yardımıyla tek fonksiyon haline getirelecek
 
-// }
+
+
+// interior nav slide part function
+var slideIndex = 1;
+showDivs1(slideIndex);
+
+function plusDivs1(n) {
+    showDivs1(slideIndex += n);
+}
+
+function showDivs1(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides1");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
